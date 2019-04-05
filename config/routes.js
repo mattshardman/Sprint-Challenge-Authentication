86dd83485d85a2fdf4bbc2948da4e076f98b5e53
mattshardman <mattshardman@gmail.com>
2019-04-05 09:02:10 +0100

    add register route to routes config

--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,23 @@
 const axios = require("axios");
 
-const { authenticate } = require("../auth/authenticate");
+const { hashSync } = require("bcryptjs");
 
-function login(req, res) {
-    // implement user login
-}
+const Users = require("../database/helpers/userModel");
+const { authenticate, createToken } = require("../auth/authenticate");
+
+const register = async (req, res) => {
+    const { username, password } = req.body;
+
+    const hash = hashSync(password, 10);
+
+    try {
+        const user = await Users.add({ username, password: hash });
+        const token = createToken({ id: user.id, user: user.username });
+        res.status(201).json(token);
+    } catch (e) {
+        res.status(401).json({ message: "Failed to add user" });
+    }
+};
 
 function getJokes(req, res) {
     const requestOptions = {
