996dbf62d270c5b772dc257472f0d341a9a42a23
mattshardman <mattshardman@gmail.com>
2019-04-05 09:42:46 +0100

    add jokes component to react app and call api with auth using local storage jwt

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from './components/Login';
+import Jokes from './components/Jokes';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Route path="/login" component={Login} />
+        <Route path="/jokes" component={Jokes} />
       </Router>
     );
   }
--- client/src/components/Jokes.js
+++ client/src/components/Jokes.js
@@ -1,1 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 100%:
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+`;
+
+const JokeWrapper = styled.div`
+  width: 600px;
+  max-width: 100%;
+  padding: 10px;
+`;
+
+function Jokes() {
+  const [jokes, setJokes] = useState([]);
+  const getJokes = async token => {
+    const results = await axios.get("http://localhost:3300/api/jokes", {
+      headers: { Authorization: token }
+    });
+    console.log(results)
+    setJokes(results.data);
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("auth_token");
+    if (token) {
+      getJokes(token);
+    }
+  }, []);
+  return (
+    <Container>
+      <JokeWrapper>
+          {jokes.map(({joke}) => <div>{joke}</div>)}
+          </JokeWrapper>
+    </Container>
+  );
+}
+
+export default Jokes;
